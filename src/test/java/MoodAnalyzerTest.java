2e5be6c54cb01fba14a794e7730e4ea62f127a98
rimadas123 <rdas5969@gmail.com>
2020-03-07 19:03:08 +0530

    Added : Analyse mood method will return happy or sad with a passed test case

--- src/test/java/MoodAnalyzerTest.java
+++ src/test/java/MoodAnalyzerTest.java
@@ -3,10 +3,18 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class MoodAnalyzerTest {
+
     @Test
     public void givenHappyMood_whenProper_ShouldReturnHappy() {
-        MoodAnalyzer moodAnalyzer = new MoodAnalyzer();
+        MoodAnalyzer moodAnalyzer = new MoodAnalyzer("I'm Happy");
         String mood = moodAnalyzer.createMoodAnalyzer();
         Assert.assertEquals("Happy", mood);
+    }
+
+    @Test
+    public void givenSadMood_whenProper_ShouldReturnSad() {
+        MoodAnalyzer moodAnalyzer = new MoodAnalyzer("I'm Sad");
+        String mood = moodAnalyzer.createMoodAnalyzer();
+        Assert.assertEquals("Sad", mood);
     }
 }
