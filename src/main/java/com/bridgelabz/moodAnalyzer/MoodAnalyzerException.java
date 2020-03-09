fa69268a5eb1eb0cd9199e4db4e033ab73d10645
rimadas123 <rdas5969@gmail.com>
2020-03-09 15:58:05 +0530

    Added : created custom exception and used enum to differentiate the mood analysis error

--- src/main/java/com/bridgelabz/moodAnalyzer/MoodAnalyzerException.java
+++ src/main/java/com/bridgelabz/moodAnalyzer/MoodAnalyzerException.java
@@ -1,4 +1,14 @@
 package com.bridgelabz.moodAnalyzer;
 
-public class MoodAnalyzerException {
+public class MoodAnalyzerException extends Exception{
+
+   public enum ExceptionType {
+        ENTERED_NULL,ENTERED_EMPTY
+    }
+   public ExceptionType exceptiontype;
+
+   public MoodAnalyzerException(ExceptionType exceptionType, String message) {
+        super(message);
+        this.exceptiontype=exceptionType;
+    }
 }
