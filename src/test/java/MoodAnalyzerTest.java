34bf57393d7ff39e906835387b5303a500d89185
rimadas123 <rdas5969@gmail.com>
2020-03-09 16:05:02 +0530

    Added : User has passed null that throw custom exception

--- src/test/java/MoodAnalyzerTest.java
+++ src/test/java/MoodAnalyzerTest.java
@@ -1,30 +1,32 @@
 import com.bridgelabz.moodAnalyzer.MoodAnalyzer;
+import com.bridgelabz.moodAnalyzer.MoodAnalyzerException;
 import org.junit.Assert;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 
 public class MoodAnalyzerTest {
 
     @Test
-    public void givenHappyMood_whenProper_ShouldReturnHappy() {
+    public void givenHappyMood_whenProper_ShouldReturnHappy() throws MoodAnalyzerException {
         MoodAnalyzer moodAnalyzer = new MoodAnalyzer("I'm Happy");
         String mood = moodAnalyzer.createMoodAnalyzer();
         Assert.assertEquals("Happy", mood);
     }
 
     @Test
-    public void givenSadMood_whenProper_ShouldReturnSad() {
+    public void givenSadMood_whenProper_ShouldReturnSad() throws MoodAnalyzerException {
         MoodAnalyzer moodAnalyzer = new MoodAnalyzer("I'm Sad");
         String mood = moodAnalyzer.createMoodAnalyzer();
         Assert.assertEquals("Sad", mood);
     }
 
     @Test
-    public void givenNullShouldReturnHappy() {
+    public void givenNullShouldThrowException() {
+        MoodAnalyzer moodAnalyzer = new MoodAnalyzer(null);
         try {
-            MoodAnalyzer moodAnalyzer = new MoodAnalyzer(null);
             moodAnalyzer.createMoodAnalyzer();
-        } catch (NullPointerException e) {
-            Assert.assertEquals("Happy", null);
+        } catch (MoodAnalyzerException e) {
+            Assert.assertEquals(MoodAnalyzerException.ExceptionType.ENTERED_NULL,e.exceptiontype);
         }
     }
 
