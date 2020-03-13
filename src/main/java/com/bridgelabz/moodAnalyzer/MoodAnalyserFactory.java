eee5a8218bb6debdc2d2c84bbc2e3727251e0c5b
rimadas123 <rdas5969@gmail.com>
2020-03-13 13:03:47 +0530

    Added : getConstructor method to check the improper class name and thrown exception

--- src/main/java/com/bridgelabz/moodAnalyzer/MoodAnalyserFactory.java
+++ src/main/java/com/bridgelabz/moodAnalyzer/MoodAnalyserFactory.java
@@ -7,12 +7,23 @@ public class MoodAnalyserFactory {
     // Default constructor
     public static MoodAnalyzer createMoodAnalyser() {
         try {
-            Constructor constructor = Class.forName("com.bridgelabz.moodAnalyzer.MoodAnalyzer").getConstructor(String.class);
+            Constructor constructor = Class.forName("com.bridgelabz.moodAnalyzer.MoodAnalyzer").getConstructor();
             Object obj = constructor.newInstance();
             return (MoodAnalyzer) obj;
         } catch (Exception e) {
             e.printStackTrace();
         }
+        return null;
+    }
+    // To check improper class name
+    public static MoodAnalyzer getConstructor(String s, Class cls) throws MoodAnalyzerException {
+            try {
+                Constructor constructor = Class.forName("com.bridgelabz.moodAnalyzer.MoodAnalyzer").getConstructor();
+            } catch (NoSuchMethodException e) {
+                e.printStackTrace();
+            } catch (ClassNotFoundException e) {
+                throw new MoodAnalyzerException(MoodAnalyzerException.ExceptionType.NO_SUCH_CLASS,e.getMessage());
+            }
         return null;
     }
 }
