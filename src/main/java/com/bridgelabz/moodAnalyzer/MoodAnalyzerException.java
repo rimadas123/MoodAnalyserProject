f024f17d5313f188f6c9952258dcbdd99ec5a941
rimadas123 <rdas5969@gmail.com>
2020-03-13 13:06:07 +0530

    Added : NO_SUCH_CLASS in enum method

--- src/main/java/com/bridgelabz/moodAnalyzer/MoodAnalyzerException.java
+++ src/main/java/com/bridgelabz/moodAnalyzer/MoodAnalyzerException.java
@@ -3,7 +3,7 @@ package com.bridgelabz.moodAnalyzer;
 public class MoodAnalyzerException extends Exception{
 
    public enum ExceptionType {
-        ENTERED_NULL,ENTERED_EMPTY
+        ENTERED_NULL,ENTERED_EMPTY,NO_SUCH_CLASS
     }
 
    public ExceptionType exceptiontype;
