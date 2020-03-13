bf18dcd726be14777d0d22cb0ccb4ba25a338687
rimadas123 <rdas5969@gmail.com>
2020-03-13 15:21:06 +0530

    Added : getTheConstructor method to check improper constructor parameter

--- src/main/java/com/bridgelabz/moodAnalyzer/MoodAnalyserFactory.java
+++ src/main/java/com/bridgelabz/moodAnalyzer/MoodAnalyserFactory.java
@@ -15,15 +15,30 @@ public class MoodAnalyserFactory {
         }
         return null;
     }
+
     // To check improper class name
-    public static MoodAnalyzer getConstructor(String s, Class cls) throws MoodAnalyzerException {
-            try {
-                Constructor constructor = Class.forName("com.bridgelabz.moodAnalyzer.MoodAnalyzer").getConstructor();
-            } catch (NoSuchMethodException e) {
-                e.printStackTrace();
-            } catch (ClassNotFoundException e) {
-                throw new MoodAnalyzerException(MoodAnalyzerException.ExceptionType.NO_SUCH_CLASS,e.getMessage());
-            }
+    public static MoodAnalyzer getClass(String s, Class cls) throws MoodAnalyzerException {
+        try {
+            Constructor constructor = Class.forName("com.bridgelabz.moodAnalyzer.MoodAnalyzer").getConstructor();
+        } catch (NoSuchMethodException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            throw new MoodAnalyzerException(MoodAnalyzerException.ExceptionType.NO_SUCH_CLASS, e.getMessage());
+        }
+        return null;
+    }
+
+    //To check improper constructor parameter
+    public static MoodAnalyzer getTheConstructor(String message, Class<String> stringClass) throws MoodAnalyzerException {
+        try {
+            Constructor constructor = Class.forName("com.bridgelabz.moodAnalyzer.MoodAnalyzer").getConstructor();
+            Object instance = constructor.newInstance();
+            return (MoodAnalyzer) instance;
+        } catch (NoSuchMethodException | InstantiationException | IllegalAccessException | InvocationTargetException e) {
+            throw new MoodAnalyzerException(MoodAnalyzerException.ExceptionType.NO_SUCH_METHOD,e.getMessage());
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
         return null;
     }
 }
