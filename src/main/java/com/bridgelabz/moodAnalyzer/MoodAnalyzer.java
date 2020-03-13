74d32384f7bd3bbb136dc610b934b9d477757b6b
rimadas123 <rdas5969@gmail.com>
2020-03-13 11:29:32 +0530

    Added : Classname should return class object and check if equals

--- src/main/java/com/bridgelabz/moodAnalyzer/MoodAnalyzer.java
+++ src/main/java/com/bridgelabz/moodAnalyzer/MoodAnalyzer.java
@@ -1,21 +1,28 @@
 package com.bridgelabz.moodAnalyzer;
 
 import java.util.EmptyStackException;
+import java.util.Objects;
 
 public class MoodAnalyzer {
 
-    String message, mood;
+    private String message;
+    String mood;
 
     public MoodAnalyzer(){}
 
     public MoodAnalyzer(String message) {
-        this.message = message;
+        this.message=message;
     }
 
-    public String createMoodAnalyzer() throws MoodAnalyzerException{
+    public String MoodAnalyzer(String message) throws MoodAnalyzerException {
+        this.message = message;
+        return message;
+    }
+
+    public String createMoodAnalyzer() throws MoodAnalyzerException {
         try {
             if (message.length() == 0){
-                mood="";
+                throw new MoodAnalyzerException(MoodAnalyzerException.ExceptionType.ENTERED_EMPTY,"Please enter proper mood");
             }
             if (message.contains("Happy")) {
                 mood = "Happy";
@@ -26,5 +33,19 @@ public class MoodAnalyzer {
         } catch (NullPointerException e) {
             throw new MoodAnalyzerException(MoodAnalyzerException.ExceptionType.ENTERED_NULL,"Please enter proper mood");
         }
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        MoodAnalyzer that = (MoodAnalyzer) o;
+        return Objects.equals(message, that.message) &&
+                Objects.equals(mood, that.mood);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(message, mood);
     }
 }
