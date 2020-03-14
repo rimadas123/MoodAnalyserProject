9bebf12957bc77a57455956f493779fae60aeb3d
rimadas123 <rdas5969@gmail.com>
2020-03-14 23:40:27 +0530

    Added : No_Such_Field constant in enum method

--- src/main/java/com/bridgelabz/moodAnalyzer/MoodAnalyzerException.java
+++ src/main/java/com/bridgelabz/moodAnalyzer/MoodAnalyzerException.java
@@ -3,7 +3,7 @@ package com.bridgelabz.moodAnalyzer;
 public class MoodAnalyzerException extends Exception{
 
    public enum ExceptionType {
-        ENTERED_NULL,ENTERED_EMPTY,NO_SUCH_CLASS,NO_SUCH_METHOD
+        ENTERED_NULL,ENTERED_EMPTY,NO_SUCH_CLASS,NO_SUCH_METHOD,NO_SUCH_FIELD
     }
 
    public ExceptionType exceptiontype;
